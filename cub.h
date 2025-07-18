e61600d9f73bd117e0bd76f5ea33e0d85b5bd526
Abdessamad Bouabbadi <abouabba@e3r11p8.1337.ma>
2025-07-18 20:32:35 +0100

    first push

--- cub.h
+++ cub.h
@@ -3,61 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   cub.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rlamlaik <rlamlaik@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abouabba <abouabba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 02:56:59 by rlamlaik          #+#    #+#             */
-/*   Updated: 2025/07/17 09:06:46 by rlamlaik         ###   ########.fr       */
+/*   Updated: 2025/07/18 03:56:59 by abouabba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef CUB_H
 # define CUB_H
 
 #include <stdio.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
 #include "mlx.h"
 
-
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
-#define FOV (M_PI / 3)  // 60 degrees Field of View
-
-#define TWO_PI 6.28318530718  // 2 * PI
-
-typedef struct s_player t_player;
-typedef struct s_screen t_screen;
-
-
-typedef struct s_screen 
-{
-    t_player *player;
-	void	*init;
-	void	*wind;
-	void	*img;
-    char	*addr;
-	int		bits_for_pixel;
-	int		line_length;
-	int		endian;
-
-
-} t_screen;
+typedef struct s_textures {
+	char *north;
+	char *south;
+	char *west;
+	char *east;
+} t_textures;
 
 typedef struct s_player {
-    t_screen *window;
-    double x_axes;
-    double y_axes;
-    double angle;         // Player's view angle
-    double x_dir;         // X direction unit vector
-    double y_dir;         // Y direction unit vector
-    double step_size;
-    char **map;
+	int x;
+	int y;
+	char direction; // 'N', 'S', 'E', or 'W'
 } t_player;
 
-int	init_window(t_screen *window);
+typedef struct s_game {
+	t_textures textures;
+	int floor_color;
+	int ceiling_color;
+	char **map;
+	int map_width;
+	int map_height;
+	t_player player;
+} t_game;
 
 
 #endif
